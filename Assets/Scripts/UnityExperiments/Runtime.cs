93b65ffd0386af091d28c564de82217f287e9d72
Marcus R. Brown <me@igetgam.es>
2017-02-04 14:52:27 -0700

    Add Runtime script.

---
+++ Assets/Scripts/UnityExperiments/Runtime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public class Runtime : MonoBehaviour
+{
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        // From http://answers.unity3d.com/answers/333787/view.html
+        var type = Type.GetType("Mono.Runtime");
+        if (type != null)
+        {
+            var displayName = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
+            if (displayName != null)
+            {
+                Debug.Log(displayName.Invoke(null, null));
+            }
+        }
+    }
+}
